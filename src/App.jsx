d9e532e609697852fef16e98b3cef8be0317bddf
Sukhdeep Singh <sukh.butalia@gmail.com>
2022-05-19 08:45:49 +0530

    Add route for user profile

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { GithubPovider } from './context/GithubContext';
 import Header from './components/layout/Header';
+import User from './components/User';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
         <div className="app">
           <Routes>
             <Route path="/" element={<Header />} />
+            <Route path="users/:login" element={<User />} />
           </Routes>
         </div>
       </Router>
